c760a101058e778e9bde51630072e07e30e40903
Jason <machjas@outlook.com>
2024-09-20 21:35:10 -0500

    edit configuration for headless server

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -15,15 +15,8 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "forge"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
+  # NixOS Declared Version
+  system.stateVersion = "24.05";
 
   # Set your time zone.
   time.timeZone = "America/Chicago";
@@ -43,39 +36,23 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+  # Enable networking
+  networking.networkmanager.enable = true;
 
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+# Firewall
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+  
+# Users
   users.users.jas = {
     isNormalUser = true;
     description = "Jason";
@@ -87,38 +64,26 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # Packages - System Profile
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+   neovim
+   wget
+   curl
+   git
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
+# SUID Wrappers
   # programs.mtr.enable = true;
   # programs.gnupg.agent = {
   #   enable = true;
   #   enableSSHSupport = true;
   # };
 
-  # List services that you want to enable:
+# Services
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  # OpenSSH
+  services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
-
+  # Tailscale
+  services.tailscale.enable = true;
 }
--- scripts/0-fresh-dotfiles-init.sh
+++ scripts/0-fresh-dotfiles-init.sh
@@ -6,12 +6,12 @@ mkdir -p ~/dotfiles
     # NixOS Configuration Files
     echo "Moving NixOS Configuration Files..."
 
-    sudo mv /etc/nixos/configuration.nix $HOME/dotfiles/nixos/
-    sudo mv /etc/nixos/hardware-configuration.nix $HOME/dotfiles/nixos/
+    sudo mv /etc/nixos/configuration.nix ~/dotfiles/nixos/
+    sudo mv /etc/nixos/hardware-configuration.nix ~/dotfiles/nixos/
 
     # NixOS Home Manager Configuration Files
     echo "Moving Home Manager Configuration Files..."
 
-    mv $HOME/.config/home-manager/home.nix
-    mv $HOME/.config/home-manager/flake.nix
-    mv $HOME/.config/home-manager/flake.lock
+    mv ~/.config/home-manager/home.nix
+    mv ~/.config/home-manager/flake.nix
+    mv ~/.config/home-manager/flake.lock
