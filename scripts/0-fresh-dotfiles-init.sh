6fda101a5f1e197a4dda685532767b801a409239
machtendo <93202234+machtendo@users.noreply.github.com>
2024-09-23 10:19:20 -0500

    Update 0-fresh-dotfiles-init.sh

--- scripts/0-fresh-dotfiles-init.sh
+++ scripts/0-fresh-dotfiles-init.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
-mkdir -p ~/dotfiles
+mkdir -p ~/.dotfiles
 
 # Move Configuration Files to 'dotfiles'
 
     # NixOS Configuration Files
     echo "Moving NixOS Configuration Files..."
 
-    sudo mv /etc/nixos/configuration.nix ~/dotfiles/nixos/
-    sudo mv /etc/nixos/hardware-configuration.nix ~/dotfiles/nixos/
+    sudo mv /etc/nixos/configuration.nix ~/.dotfiles/nixos/configuration.nix
+    sudo mv /etc/nixos/hardware-configuration.nix ~/.dotfiles/nixos/hardware-configuration.nix
 
     # NixOS Home Manager Configuration Files
     echo "Moving Home Manager Configuration Files..."
 
-    mv ~/.config/home-manager/home.nix
-    mv ~/.config/home-manager/flake.nix
-    mv ~/.config/home-manager/flake.lock
+    mv ~/.config/home-manager/home.nix ~/.dotfiles/nixos/home.nix
+    mv ~/.config/home-manager/flake.nix ~/.dotfiles/nixos/flake.nix
+    mv ~/.config/home-manager/flake.lock ~/.dotfiles/nixos/flake.lock
