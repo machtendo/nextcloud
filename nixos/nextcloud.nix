4174cbc2b4f5fa3ad8a4c21c2cfd71e27f35a9be
machtendo <93202234+machtendo@users.noreply.github.com>
2024-10-09 17:22:15 -0500

    no longer has any effect

--- nixos/nextcloud.nix
+++ nixos/nextcloud.nix
@@ -30,7 +30,6 @@
       # Increase the maximum file upload size to avoid problems uploading videos.
       maxUploadSize = "16G";
       https = true;
-      enableBrokenCiphersForSSE = false;
 
       autoUpdateApps.enable = true;
       extraAppsEnable = true;
